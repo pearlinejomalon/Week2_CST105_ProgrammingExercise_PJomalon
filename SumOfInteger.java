55ac964fdb98272b551f1cd2c462c05a323e8f04
pearlinejomalon <43321994+pearlinejomalon@users.noreply.github.com>
2018-09-17 00:14:22 -0500

    Add files via upload

---
+++ SumOfInteger.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumOfInteger {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		System.out.print("Enter 5-digit number:");
+		int number = scanner.nextInt();
+		int sumOfInteger = findSumOfInteger (number);
+		System.out.print("Sum of 5-digit number is:" + sumOfInteger);
+		
+		scanner.close();
+	
+	}
+	public static int findSumOfInteger (int number)  {
+		int sum = 0;
+		while (number > 0)  {
+			int digit = number % 10;
+			number = number / 10;
+			sum += digit;
+			
+		}
+		return sum;
+	}
+
+}
